8ce46b622e9f06dc0893b70862a230b8d4afe24c
mefuru <mehul.mandania@gmail.com>
2021-08-23 16:39:34 +0200

    Source transactions

--- packages/emissions-dashboard/pages/index.tsx
+++ packages/emissions-dashboard/pages/index.tsx
@@ -3,30 +3,81 @@ import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 const EthDater = require('ethereum-block-by-date');
 
+const UNISWAP_V2_CONTRACT_ADDRESSES =
+  '0x7a250d5630b4cf539739df2c5dacb4c659f2488d';
+const START_DATE = '2021-08-20T00:00:00Z';
+const END_DATE = '2021-08-20T00:02:00Z';
+
 const IndexPage = () => {
   const router = useRouter();
   const [provider, setProvider] = useState<InfuraProvider>();
   const [dater, setDater] = useState();
+  const [startBlock, setStartBlock] = useState<string>();
+  const [endBlock, setEndBlock] = useState<string>();
+  const [blocksInRange, setBlocksInRange] = useState<number[]>([]);
+  const [transactions, setTransactions] = useState([]);
   useEffect(() => {
     if (typeof window !== 'undefined' && window.location.pathname !== '/') {
       router.replace(window.location.pathname);
     }
   }, [router.pathname]);
 
-  const initializeProvider = async () => {
+  const initialize = async () => {
     console.log('Setting up Infura provider...');
     const provider = new InfuraProvider('homestead', {
       projectId: process.env.INFURA_PROJECT_ID,
       projectSecret: process.env.INFURA_PROJECT_SECRET,
     });
     const dater = new EthDater(provider);
+
     setProvider(provider);
     setDater(dater);
   };
 
+  const getBlocks = async () => {
+    const startBlock = await (await dater.getDate(START_DATE)).block;
+    const endBlock = await (await dater.getDate(END_DATE)).block;
+    setStartBlock(startBlock);
+    setEndBlock(endBlock);
+    setBlocksInRange(
+      new Array(endBlock - startBlock)
+        .fill(undefined)
+        .map((x, i) => startBlock + i),
+    );
+  };
+
+  const getTransactions = async () => {
+    const blockData = await Promise.all(
+      blocksInRange.map(async (block) => await provider.getBlock(block)),
+    );
+    const txs = blockData.map((block) => block.transactions).flat();
+    const transactions = await Promise.all(
+      txs.map(async (tx) => await provider.getTransaction(tx)),
+    );
+    setTransactions(transactions);
+  };
+
   useEffect(() => {
-    initializeProvider();
+    initialize();
   }, []);
+
+  useEffect(() => {
+    if (dater !== undefined && provider !== undefined) {
+      getBlocks();
+    }
+  }, [provider, dater]);
+
+  useEffect(() => {
+    if (startBlock !== undefined && endBlock !== undefined) {
+      getTransactions();
+    }
+  }, [startBlock, endBlock]);
+
+  useEffect(() => {
+    if (blocksInRange.length > 0) {
+      getTransactions();
+    }
+  }, [startBlock, endBlock]);
 
   return (
     <div
