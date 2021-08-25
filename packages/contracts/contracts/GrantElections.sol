84d6be6404bfb5d558f38fd12600384be8b861c5
mefuru <mehul.mandania@gmail.com>
2021-08-25 17:55:44 +0200

    Remove keeperIncentive from refreshElection

--- packages/contracts/contracts/GrantElections.sol
+++ packages/contracts/contracts/GrantElections.sol
@@ -283,7 +283,7 @@ contract GrantElections is ParticipationReward, KeeperIncentive {
     emit BeneficiaryRegistered(_beneficiary, _electionId);
   }
 
-  function refreshElectionState(uint256 _electionId) public keeperIncentive(0) {
+  function refreshElectionState(uint256 _electionId) public {
     Election storage election = elections[_electionId];
     if (
       block.timestamp >=
