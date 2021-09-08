d53979b662b559aeccf44b86012df5a9725847c3
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-09-08 16:25:10 +0100

    first section on mobile landing page can be longer than 1 screen size for smaller phones

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -660,7 +660,7 @@ const IndexPage = () => {
             <Menu onClick={(e) => toggleMenu(true)} />
           </nav>
         </header>
-        <section className="max-h-screen min-h-full">
+        <section className="min-h-full">
           <div className="bg-primary w-full h-12"></div>
           <div
             className="bg-hero-pattern flex-shrink-0 flex-grow-0 w-full h-full"
