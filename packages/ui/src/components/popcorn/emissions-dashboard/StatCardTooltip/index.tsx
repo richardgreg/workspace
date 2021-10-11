cb54476755cad77d879183ece4dcc2184343d6fe
mefuru <mehul.mandania@gmail.com>
2021-10-11 15:31:55 +0200

    Update design + spacing

--- packages/ui/src/components/popcorn/emissions-dashboard/StatCardTooltip/index.tsx
+++ packages/ui/src/components/popcorn/emissions-dashboard/StatCardTooltip/index.tsx
@@ -23,21 +23,30 @@ const StatCardTooltip: React.FC<StatCardTooltipProps> = ({
       type="light"
       className="rounded-lg shadow-lg border border-gray-50 p-1 w-84 "
     >
-      <p className="font-medium ">Previous Period</p>
-      <p className="text-sm font-light text-gray-500 ">
-        {'Start: ' + previousPeriodStartDate.toLocaleDateString()}
-      </p>
-      <p className="text-sm font-light text-gray-500 ">
-        {'End: ' + startDate.toLocaleDateString()}
-      </p>
+      <p className="font-bold ">Previous Period</p>
 
-      <p className="font-medium">
-        {item.name === 'Transactions' &&
-          item.statPrev.toLocaleString() + ' transactions'}
-        {item.name === 'Average Gas Price' && item.statPrev.toLocaleString()}
-        {item.name.includes('CO2 Emissions') &&
-          item.statPrev.toLocaleString() + ' ' + item.name.split(' ')[2]}
-      </p>
+      <div className="flex flex-row">
+        <p className="text-sm font-light text-gray-500 mr-2">Start :</p>
+        <p className="text-sm font-light text-gray-500 ">
+          {previousPeriodStartDate.toLocaleDateString()}
+        </p>
+      </div>
+      <div className="flex flex-row">
+        <p className="text-sm font-light text-gray-500 mr-4">End :</p>
+        <p className="text-sm font-light text-gray-500 ">
+          {startDate.toLocaleDateString()}
+        </p>
+      </div>
+      <div className="flex flex-row">
+        <p className="font-semibold mr-1">Total :</p>
+        <p className="font-semibold">
+          {item.name === 'Transactions' &&
+            item.statPrev.toLocaleString() + ' transactions'}
+          {item.name === 'Average Gas Price' && item.statPrev.toLocaleString()}
+          {item.name.includes('CO2 Emissions') &&
+            item.statPrev.toLocaleString() + ' ' + item.name.split(' ')[2]}
+        </p>
+      </div>
     </ReactTooltip>
   );
 };
