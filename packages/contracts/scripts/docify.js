6483d98099e751d75c99c32f879d82b8437edf23
Soptq <32592090+soptq@users.noreply.github.com>
2021-10-19 14:59:18 +0800

    Test if surya path is correct;

--- packages/contracts/scripts/docify.js
+++ packages/contracts/scripts/docify.js
@@ -113,16 +113,25 @@ function generateGraphs(sourcePathNameList) {
     const outputInheritancePathName = OUTPUT_IMAGES_DIR + `/${contractName}_inheritance_graph.png`;
     const suryaPath = GLOBAL_NODE_DIR + "/surya/bin/surya";
 
+    exec(`${suryaPath} graph ${inputContractPathName}`, (err, stdout, stderr) => {
+      if (err) {
+        //some err occurred
+        console.error(`exec error: ${err}`);
+      }
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+
     exec(`${suryaPath} graph ${inputContractPathName} | dot -Tpng > ${outputGraphPathName}`, (err, stdout, stderr) => {
       if (err) {
         //some err occurred
-        console.error(err)
+        console.error(`exec error: ${err}`);
       }
     });
     exec(`${suryaPath} inheritance ${inputContractPathName} | dot -Tpng > ${outputInheritancePathName}`, (err, stdout, stderr) => {
       if (err) {
         //some err occurred
-        console.error(err)
+        console.error(`exec error: ${err}`);
       }
     });
   }
