297fada011807eaa8375a77b254d4ed045eec1c7
mefuru <mehul.mandania@gmail.com>
2021-11-01 18:17:46 +0100

    init LBP package

---
+++ packages/liquidity-bootstraping-pool/pages/_app.tsx
@@ -0,0 +1,97 @@
+import { GlobalLinearProgress } from 'containers/GlobalLinearProgress';
+import Head from 'next/head';
+import Router from 'next/router';
+import React, { useEffect, useState } from 'react';
+import TagManager from 'react-gtm-module';
+import { ThemeProvider } from 'styled-components';
+import '../styles/globals.css';
+import theme from '../styles/theme';
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_GTM_ID) {
+      TagManager.initialize({
+        gtmId: process.env.NEXT_PUBLIC_GTM_ID,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', () => {
+      setLoading(true);
+    });
+    Router.events.on('routeChangeComplete', () => {
+      setLoading(false);
+    });
+    Router.events.on('routeChangeError', () => {
+      setLoading(false);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Popcorn - DeFi for the People</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+
+        <meta
+          name="description"
+          content="Earn high yield on your cryptoassets while creating real world impact. Our fees go directly towards non-profits"
+        />
+
+        {/*  Facebook Meta Tags */}
+        <meta property="og:url" content="https://popcorn.network/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Popcorn - DeFi for the People" />
+        <meta
+          property="og:description"
+          content="Earn high yield on your cryptoassets while creating real world impact. Our fees go directly towards non-profits of your choosing."
+        />
+        <meta
+          property="og:image"
+          content="https://popcorn.network/images/popcorn_network_rocket_2.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="popcorn.network" />
+        <meta property="twitter:url" content="https://popcorn.network/" />
+        <meta name="twitter:title" content="Popcorn - DeFi for the People" />
+        <meta
+          name="twitter:description"
+          content="Earn high yield on your cryptoassets while creating real world impact. Our fees go directly towards non-profits of your choosing."
+        />
+        <meta
+          name="twitter:image"
+          content="https://popcorn.network/images/popcorn_network_rocket_2.png"
+        />
+
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <GlobalLinearProgress visible={loading} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
---
+++ packages/liquidity-bootstraping-pool/tailwind.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './containers/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        18: '4.5rem',
+        72: '18rem',
+        84: '21rem',
+        96: '24rem',
+        100: '25.5rem',
+        104: '27rem',
+        112: '30rem',
+        128: '40rem',
+        129: '52rem',
+      },
+      lineHeight: {
+        button: '32px',
+      },
+      scale: {
+        101: '1.01',
+        102: '1.02',
+      },
+      rotate: {
+        '-30': '-30deg',
+      },
+      colors: {
+        primary: '#FDEAA7',
+        primaryLight: '#FFF5CF',
+
+        secondary: '#73B7EA',
+        secondaryLight: '#DBEAFE',
+        secondaryDark: "#25283D",
+
+        ctaYellow: '#F6CB22',
+        ctaYellowLight: '#FFD324',
+      },
+      backgroundImage: (theme) => ({
+        'hero-pattern': "url('/images/bgHero.svg')",
+        'impact-pattern': "url('/images/bgImpact.svg')",
+        'countdown-pattern': "url('/images/bgCountdown.svg')",
+        'popcorn1-pattern': "url('/images/bgPopcorn1.svg')",
+        'popcorn2-pattern': "url('/images/bgPopcorn2.svg')",
+        'popcorn3-pattern': "url('/images/bgPopcorn3.svg')",
+      }),
+      fontFamily: {
+        landing: ['Avenir Next LT Pro', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: { opacity: ['disabled'] },
+  },
+  plugins: [],
+};
