202810c4570de5136304e2df8cf219bd004eaff8
RedVeil <leon.niesler@gmail.com>
2021-11-07 17:02:04 +0100

    purge updated 2

--- packages/liquidity-bootstraping-pool/tailwind.config.js
+++ packages/liquidity-bootstraping-pool/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
     './pages/*.{js,ts,jsx,tsx}',
-    './container/*.{js,ts,jsx,tsx}',
+    './container/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
