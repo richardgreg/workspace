b19a05132f85e7d9364ed4c63170f772a627a75e
Amirjab21 <30866671+Amirjab21@users.noreply.github.com>
2021-11-11 17:08:27 +0000

    update linked tracker so its compatible with NextJS

--- packages/app/components/LinkedInPixel.tsx
+++ packages/app/components/LinkedInPixel.tsx
@@ -3,9 +3,26 @@ interface LinkedInPixelProps {
 }
 
 export default function LinkedInPixel({conversionId} : LinkedInPixelProps): JSX.Element {
+
   return (
     <>
-      <img height="1" width="1" style={{display: 'none'}} alt="" src={`https://px.ads.linkedin.com/collect/?pid=3379538&conversionId=${conversionId}&fmt=gif`} />
+    <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+      {f._linkedin_data_partner_ids.push('3379538'); if (!f.lintrk) {
+      f.linktrk= function(a,b) { f.linktrk.q.push([a,b])};f.linktrk.q=[]}
+      t=b.createElement(e);s=b.getElementsByTagName(e)[0];
+      t.src=v;t.type="text/javascript";t.async=true;
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://snap.licdn.com/li.lms-analytics/insight.min.js');`,
+        }}
+      />
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none"
+      src="https://px.ads.linkedin.com/collect/?pid=3379538&conversionId=${conversionId}&fmt=gif" />`,
+        }}
+      />
     </>
 
   );
