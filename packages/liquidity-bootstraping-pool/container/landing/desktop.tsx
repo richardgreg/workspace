bfc23cad817f131f675d598940bac82882905274
RedVeil <44397805+RedVeil@users.noreply.github.com>
2021-11-28 15:04:05 +0100

    Update desktop.tsx

--- packages/liquidity-bootstraping-pool/container/landing/desktop.tsx
+++ packages/liquidity-bootstraping-pool/container/landing/desktop.tsx
@@ -32,7 +32,7 @@ export default function Desktop(): JSX.Element {
                       <div className="w-full h-14 bg-yellow-500 shadow-md flex justify-center cursor-pointer hover:bg-yellow-400">
                         <div className="flex flex-row items-center mx-auto">
                           <p className="text-white text-2xl font-bold">
-                            Token Launch Auction
+                            Token Launch Auction Now Live!
                           </p>
                           <Icon.ArrowRightCircle className="ml-2 w-7 h-7 text-white" />
                         </div>
