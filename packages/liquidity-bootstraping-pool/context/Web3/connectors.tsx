6845c85b8ed7bee0b0510d8eae313dbe30bddb57
RedVeil <44397805+RedVeil@users.noreply.github.com>
2021-11-29 08:48:34 +0100

    added chains (#742)

--- packages/liquidity-bootstraping-pool/context/Web3/connectors.tsx
+++ packages/liquidity-bootstraping-pool/context/Web3/connectors.tsx
@@ -2,7 +2,10 @@ import { InjectedConnector } from '@web3-react/injected-connector';
 import { NetworkConnector } from '@web3-react/network-connector';
 
 const Injected = new InjectedConnector({
-  supportedChainIds: [1, 4, 31337, 1337],
+  supportedChainIds: [
+    1, 4, 42, 69, 31337, 1337, 137, 43113, 43114, 250, 56, 97, 100, 200, 42161,
+    421611, 10,
+  ],
 });
 
 const Network = new NetworkConnector({
