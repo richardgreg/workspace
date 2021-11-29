2d138447f549dbf97f026bd467d682cd06296c84
Leon Niesler <leon.niesler@gmail.com>
2021-11-29 11:07:49 +0100

    mobile-banner fix

--- packages/liquidity-bootstraping-pool/container/landing/mobile.tsx
+++ packages/liquidity-bootstraping-pool/container/landing/mobile.tsx
@@ -37,7 +37,7 @@ export default function Mobile({
           <Link href="/auction">
             <div className="w-full h-14 bg-yellow-500 shadow-md flex justify-center cursor-pointer hover:bg-yellow-400">
               <div className="flex flex-row items-center mx-auto">
-                <p className="text-white text-2xl font-bold">
+                <p className="text-white text-xl font-bold">
                   Token Launch Auction Now Live!
                 </p>
                 <Icon.ArrowRightCircle className="ml-2 w-7 h-7 text-white" />
