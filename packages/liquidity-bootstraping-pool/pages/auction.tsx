398ff1265a165debcd1b0953c6e8107997dba047
Anthony Martin <anthony@replaycreative.com>
2021-11-30 22:56:52 +0100

    link targets new page

--- packages/liquidity-bootstraping-pool/pages/auction.tsx
+++ packages/liquidity-bootstraping-pool/pages/auction.tsx
@@ -32,9 +32,8 @@ const IndexPage = () => {
   useEffect(() => {
     if (typeof window !== 'undefined' && window.location.pathname !== '/') {
       router.replace(window.location.pathname);
-    }  
+    }
   }, [router.pathname]);
-
 
   useEffect(() => {
     if (window && !window.ethereum) {
@@ -53,8 +52,7 @@ const IndexPage = () => {
       console.log(error);
     }
     return supabase;
-  }
-
+  };
 
   useEffect(() => {
     if (account) {
@@ -64,19 +62,19 @@ const IndexPage = () => {
 
   async function acceptConditions() {
     const timestamp = Date.now();
-    const message = await library
-      .getSigner()
-      .signMessage(getTerms(timestamp));
+    const message = await library.getSigner().signMessage(getTerms(timestamp));
     if (message) {
       setStep(Step.Network);
       try {
-        await getSupabase()?.from('signedTerms').insert([
-          {
-            address: account,
-            signature: message,
-            timestamp: timestamp,
-          },
-        ]);
+        await getSupabase()
+          ?.from('signedTerms')
+          .insert([
+            {
+              address: account,
+              signature: message,
+              timestamp: timestamp,
+            },
+          ]);
         return true;
       } catch (error) {
         console.log('error', error);
@@ -88,7 +86,7 @@ const IndexPage = () => {
   const titleText = () => {
     switch (step) {
       case Step.NeedsWallet:
-        return "Wallet Required!"
+        return 'Wallet Required!';
       case Step.Wallet:
         return 'Step 1: Connect your Wallet';
       case Step.Terms:
@@ -110,11 +108,12 @@ const IndexPage = () => {
               {titleText()}
             </h2>
             {step === Step.NeedsWallet && (
-                  <div className="flex flex-col items-center">
-                  <p className="mt-8 md:mx-18 text-center">
-                    To view this page, you must have a wallet extension (like Metamask) installed on your browser.
-                  </p>
-                </div>
+              <div className="flex flex-col items-center">
+                <p className="mt-8 md:mx-18 text-center">
+                  To view this page, you must have a wallet extension (like
+                  Metamask) installed on your browser.
+                </p>
+              </div>
             )}
             {step === Step.Wallet && (
               <div className="flex flex-col items-center">
@@ -132,7 +131,10 @@ const IndexPage = () => {
             {step === Step.Terms && (
               <div className="flex flex-col items-center">
                 <p className="mt-8 md:mx-18 text-center">
-                  Please sign Terms and Conditions by clicking the button below. <span className="italic">Signing the message is free, no transaction fee is incurred.</span>
+                  Please sign Terms and Conditions by clicking the button below.{' '}
+                  <span className="italic">
+                    Signing the message is free, no transaction fee is incurred.
+                  </span>
                 </p>
                 <button
                   className="font-medium text-base bg-blue-600 hover:bg-blue-500 text-white rounded-full px-10 py-2 mt-8 cursor-pointer"
@@ -144,28 +146,35 @@ const IndexPage = () => {
             )}
             {step === Step.Network && (
               <div className="mt-8 flex flex-row items-center justify-center space-x-8">
-                <Link href="https://copperlaunch.com/auctions/0xc79b9b75cd0e9567a000eeb8f6e46b3d074ac38c">
-                  <div>
-                    <div className="flex w-32 h-32 border-2 border-gray-300 rounded-lg items-center justify-center flex-shrink-0 cursor-pointer hover:bg-blue-50 hover:border-blue-500">
-                      <img src="/images/ethLogo.svg" className="w-20 h-20" />
+                <Link
+                  href="https://copperlaunch.com/auctions/0xc79b9b75cd0e9567a000eeb8f6e46b3d074ac38c"
+                  passHref
+                >
+                  <a target="_blank">
+                    <div>
+                      <div className="flex w-32 h-32 border-2 border-gray-300 rounded-lg items-center justify-center flex-shrink-0 cursor-pointer hover:bg-blue-50 hover:border-blue-500">
+                        <img src="/images/ethLogo.svg" className="w-20 h-20" />
+                      </div>
+                      <p className="text-sm text-gray-500 text-center mt-1">
+                        Ethereum
+                      </p>
                     </div>
-                    <p className="text-sm text-gray-500 text-center mt-1">
-                      Ethereum
-                    </p>
-                  </div>
+                  </a>
                 </Link>
                 <Link href="https://polygon.copperlaunch.com/auctions/0x6d68d7b0ca469bd1171f81a895e649d86d523c20">
-                  <div>
-                    <div className="flex w-32 h-32 border-2 border-gray-300 rounded-lg items-center justify-center flex-shrink-0 cursor-pointer hover:bg-blue-50 hover:border-blue-500">
-                      <img
-                        src="/images/polygonLogo.svg"
-                        className="w-20 h-20"
-                      />
+                  <a target="_blank">
+                    <div>
+                      <div className="flex w-32 h-32 border-2 border-gray-300 rounded-lg items-center justify-center flex-shrink-0 cursor-pointer hover:bg-blue-50 hover:border-blue-500">
+                        <img
+                          src="/images/polygonLogo.svg"
+                          className="w-20 h-20"
+                        />
+                      </div>
+                      <p className="text-sm text-gray-500 text-center mt-1">
+                        Polygon
+                      </p>
                     </div>
-                    <p className="text-sm text-gray-500 text-center mt-1">
-                      Polygon
-                    </p>
-                  </div>
+                  </a>
                 </Link>
               </div>
             )}
@@ -183,7 +192,7 @@ const IndexPage = () => {
   );
 };
 
-const getTerms =(timestamp) => {
+const getTerms = (timestamp) => {
   return `
   Popcorn Token Launch Auction Terms and Conditions
 
@@ -209,6 +218,6 @@ const getTerms =(timestamp) => {
   
   Accepted: ${timestamp}
   `;
-}
+};
 
 export default IndexPage;
